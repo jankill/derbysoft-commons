15206338901ff51aeaf4a4b0d469dbcafd109d9e
zhupan <panoszhu@gmail.com>
2014-12-08 14:38:56 +0800

    r

---
+++ src/main/java/com/derbysoft/common/util/XmlFormatUtils.java
@@ -0,0 +1,10 @@
+package com.derbysoft.common.util;
+
+@Deprecated
+public abstract class XmlFormatUtils {
+
+    public static String formatXml(String value) {
+        return com.derbysoft.common.util.xml.XmlFormatUtils.formatXml(value);
+    }
+
+}
